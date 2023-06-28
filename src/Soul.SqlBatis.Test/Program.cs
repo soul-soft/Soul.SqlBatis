51f7594591556c44ff709016e68b87ceab3743fe
王剑锋 <1448376744@qq.com>
2023-06-28 13:13:06 +0800

    优化demo

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -9,9 +9,10 @@ var req = new
 var sb = new SqlBuilder();
 sb = sb.From("students join schools on students.sch_id = schools.id")
     .Where("age > @Age", req.Age != null)
-    .OrderBy("students.age desc");
+    .OrderBy("students.age desc")
+    .Page(1, 20);
 
-var querySql = sb.Page(1,20).Select("students.id,students.age,schools.name");
-var countSql = sb.Page(1, 20).Count();
+var querySql = sb.Select("students.id,students.age,schools.name");
+var countSql = sb.Count();
 Console.WriteLine(querySql);
 Console.WriteLine(countSql);
