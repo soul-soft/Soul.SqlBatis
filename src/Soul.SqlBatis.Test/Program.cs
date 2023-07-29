17d77509f760e097f495a7019e0426b82a3803bf
wang <1448376744@qq.com>
2023-07-29 16:19:59 +0800

    dev

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -4,7 +4,8 @@ using Soul.SqlBatis.Test;
 var context = new MyDbContext();
 var list = new int[] { };
 var req = new { Age = 50 };
-var sql = context.Set<Student>()
-    .Where(a=>a.Id>0)
-    .ToList();
+var sql = context.Students
+	.FromSql("students")
+	.Where(a => a.Id > 0)
+	.ToList();
 Console.WriteLine();
