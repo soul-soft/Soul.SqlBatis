abae0d8e83ee768b53e4d7e6e8c26eb93401d6fb
王剑锋 <1448376744@qq.com>
2023-08-25 09:35:01 +0800

    dev

--- src/Soul.SqlBatis/Infrastructure/Model/EntityProperty.cs
+++ src/Soul.SqlBatis/Infrastructure/Model/EntityProperty.cs
@@ -63,6 +63,8 @@ namespace Soul.SqlBatis.Infrastructure
             }
         }
 
+        public string CSharpName => Property.Name;
+
         public EntityProperty(MemberInfo member)
         {
             Property = member as PropertyInfo;
