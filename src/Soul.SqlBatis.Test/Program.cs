df55a03bcb106404e8ac25fd075a4a63b76595a0
王剑锋 <1448376744@qq.com>
2023-08-31 10:17:20 +0800

    dev

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -1,10 +1,7 @@
-﻿using System.Text.Json;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using MySqlConnector;
 using Soul.SqlBatis;
-using Soul.SqlBatis.Entities;
 using Soul.SqlBatis.Infrastructure;
-using Soul.SqlBatis.Test;
 
 var context = new MyDbContext(new DbContextOptions
 {
@@ -14,17 +11,9 @@ var context = new MyDbContext(new DbContextOptions
     }),
     DbConnection = new MySqlConnection("Server=localhost;Port=3306;User ID=root;Password=1024;Database=test")
 });
-
-var json = "{\"Orders\":{\"Id\":1,\"Name\":1}}";
-
-var model = JsonSerializer.Deserialize<QueryModel>(json);
-var student = context.Students
-    .AsTracking()
-    .First();
-student.Address = new Address
-{
-    P = "浙江",
-    C = "宁波"
-};
+var list = new List<int>();
+var students = context.Students
+    .Where(a => DbOperations.In(a.Id, list))
+    .ToList();
 var row = await context.SaveChangesAsync();
 Console.WriteLine();
