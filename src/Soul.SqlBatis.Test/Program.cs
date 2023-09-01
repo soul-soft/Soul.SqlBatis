9aea3c4a78b802d804e73db7d82474e3d3f6a5bc
王剑锋 <1448376744@qq.com>
2023-09-01 09:36:24 +0800

    dev

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -9,14 +9,14 @@ var context = new MyDbContext(new DbContextOptions
     {
         logging.AddConsole();
     }),
-    DbConnection = new MySqlConnection("Server=localhost;Port=3306;User ID=root;Password=1024;Database=test")
+    DbConnection = new MySqlConnection("Server=192.168.0.155;Port=3306;User ID=root;Password=1024;Database=test")
 });
-var list = new List<int>() 
+var list = new List<int>()
 {
 
 };
 var students = context.Students
-    .Where(a => DbOperations.In(a.Id, list))
+    .Where(a => a.CreationTime > DateTime.Now)
     .ToList();
 var row = await context.SaveChangesAsync();
 Console.WriteLine();
