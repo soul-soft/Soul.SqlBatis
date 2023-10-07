5f123f40d104593a76474e92ece1cf8a3eb79947
王剑锋 <1448376744@qq.com>
2023-10-07 21:57:50 +0800

    dev

--- src/Soul.SqlBatis.Test/MyDbContext.cs
+++ src/Soul.SqlBatis.Test/MyDbContext.cs
@@ -1,16 +1,28 @@
-﻿namespace Soul.SqlBatis.Infrastructure
+﻿using MySql.Data.MySqlClient;
+
+namespace Soul.SqlBatis.Infrastructure
 {
     public partial class MyDbContext : DbContext
 	{
-		public MyDbContext(DbContextOptions options)
-			: base(options)
-		{
+        public MyDbContext()
+        {
+            
+        }
 
-		}
+        public MyDbContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
 
-		protected override void Logging(string sql, object param)
+        protected override void Logging(string sql, object param)
 		{
 			Console.WriteLine(sql);
 		}
-	}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseConnectionFactory(() => new MySqlConnection("Server=localhost;Port=3306;User ID=root;Password=1024;Database=test"));
+        }
+    }
 }
