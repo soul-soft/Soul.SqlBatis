e205edcc7ff1155e7ad46e6a385ef62f32e53f17
wang <1448376744@qq.com>
2023-11-04 00:24:45 +0800

    dev

---
+++ src/Soul.SqlBatis/Infrastructure/ADO.NET/JsonConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Soul.SqlBatis.Infrastructure
+{
+	internal class JsonConverter
+	{
+
+		public static bool IsJsonType(Type type)
+		{
+			return type.CustomAttributes.Any(a => a.AttributeType == typeof(JsonValueAttribute));
+		}
+
+		public static string Serialize(object obj)
+		{
+			return JsonSerializer.Serialize(obj, SqlMapper.Settings.JsonSerializerOptions);
+		}
+
+		public static T Deserialize<T>(string json)
+		{
+			if (json == null)
+			{
+				if (SqlMapper.Settings.InitializeJsonArray && typeof(IJsonArray).IsAssignableFrom(typeof(T)))
+				{
+					json = "[]";
+				}
+				else
+				{
+					return default;
+				}
+			}
+			return JsonSerializer.Deserialize<T>(json, SqlMapper.Settings.JsonSerializerOptions);
+		}
+
+		public static MethodInfo GetDeserializeConverter(Type type)
+		{
+			return typeof(JsonConverter).GetMethod(nameof(Deserialize), new Type[] { typeof(string) }).MakeGenericMethod(type);
+		}
+	}
+}
