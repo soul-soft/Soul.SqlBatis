8f6583339e512bbc1df0416bb76a5a52d70b355e
王剑锋 <1448376744@qq.com>
2023-11-09 13:40:17 +0800

    dev

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -7,7 +7,15 @@ var options = new DbContextOptionsBuilder()
     //.UseConnectionFactory(() => new MySqlConnector.MySqlConnection("Server=localhost;Port=3306;User ID=root;Password=1024;Database=test") ,DBMS.MSSQL)
     .UseConnectionFactory(() => new Microsoft.Data.SqlClient.SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"), DBMS.MSSQL)
     .Build();
-TypeMapper.AddTypeMapper<DateTime, string>(Converts.DatetimtToString);
+
+
+TypeMapper.AddTypeMapper<DateTime, string>(date => 
+{
+    return date.ToString("yyyy-MM-dd");
+});
+
+
+
 using var context = new MyDbContext(options);
 context.OpenDbConnection();
 context.Students
