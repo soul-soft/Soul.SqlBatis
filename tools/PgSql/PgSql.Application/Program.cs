b30e8a21a030d7ce1c44321156f77a7e1e45081f
王剑锋 <1448376744@qq.com>
2025-02-11 13:07:36 +0800

    dev

--- tools/PgSql/PgSql.Application/Program.cs
+++ tools/PgSql/PgSql.Application/Program.cs
@@ -1,10 +1,5 @@
 ﻿using Soul.SqlBatis;
 using Soul.SqlBatis.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PgSql.Application
 {
