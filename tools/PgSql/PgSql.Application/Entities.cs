fe641f75e652cf7f900b3f7a9bfe018468b6012f
王剑锋 <1448376744@qq.com>
2025-02-20 11:04:28 +0800

    dev

--- tools/PgSql/PgSql.Application/Entities.cs
+++ tools/PgSql/PgSql.Application/Entities.cs
@@ -2,9 +2,10 @@
 
 using System;
 using Soul.SqlBatis;
+using System.Text.Json;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json;
 
 namespace Soul.SqlBatis.Entities
 {	
@@ -19,26 +20,36 @@ namespace Soul.SqlBatis.Entities
 		/// 
 		/// </summary>
 		[Key][Identity]
+		[Column("Id")]
+		[Description("")]
 		public override int? Id { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
+		[Column("chinese")]
+		[Description("")]
 		public decimal? Chinese { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
+		[Column("stu_id")]
+		[Description("")]
 		public int? StuId { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
+		[Column("math")]
+		[Description("")]
 		public decimal? Math { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
+		[Column("english")]
+		[Description("")]
 		public decimal? English { get; set; }
 	}
 
@@ -52,32 +63,37 @@ namespace Soul.SqlBatis.Entities
 		/// 
 		/// </summary>
 		[Key][Identity]
+		[Column("id")]
+		[Description("")]
 		public override int? Id { get; set; }
 		/// <summary>
 		/// 名称
 		/// </summary>
 		
+		[Column("name")]
+		[Description("名称")]
 		public string? Name { get; set; }
 		/// <summary>
 		/// 年龄
 		/// </summary>
 		
+		[Column("age")]
+		[Description("年龄")]
 		public int? Age { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
-		public decimal? M { get; set; }
+		[Column("dep_ids")]
+		[Description("")]
+		public int[]? DepIds { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
-		public float? F { get; set; }
-		/// <summary>
-		/// 
-		/// </summary>
-		
-		public JsonElement? Js { get; set; }
+		[Column("dep_names")]
+		[Description("")]
+		public string[]? DepNames { get; set; }
 	}
 
 	/// <summary>
@@ -90,11 +106,15 @@ namespace Soul.SqlBatis.Entities
 		/// 
 		/// </summary>
 		[Key]
+		[Column("id")]
+		[Description("")]
 		public override string? Id { get; set; }
 		/// <summary>
 		/// 
 		/// </summary>
 		
+		[Column("flag")]
+		[Description("")]
 		public bool? Flag { get; set; }
 	}
 }
--- tools/PgSql/PgSql.Application/Program.cs
+++ tools/PgSql/PgSql.Application/Program.cs
@@ -15,16 +15,33 @@ namespace PgSql.Application
                 {
                     Console.WriteLine(sql);
                 });
-                configure.UseEntityMapper(configureOptions => 
+                configure.UseEntityMapper(configureOptions =>
                 {
-                    configureOptions.UseTypeMapper((dr, i) => 
+                    configureOptions.UseTypeMapper((dr, i) =>
                     {
                         var json = dr.GetString(i);
                         return JsonDocument.Parse(json).RootElement;
                     });
+                    configureOptions.UseTypeMapper((dr, i) =>
+                    {
+                        return (dr as Npgsql.NpgsqlDataReader)!.GetFieldValue<int[]>(i);
+                    });
+                    configureOptions.UseTypeMapper((dr, i) =>
+                    {
+                        return (dr as Npgsql.NpgsqlDataReader)!.GetFieldValue<string[]>(i);
+                    });
                 });
                 configure.UseNpgsql(new Npgsql.NpgsqlConnection("Host=localhost;Port=5432;Username=postgres;Password=1024;Database=postgres;SSL Mode=Disable;"));
             });
+            var stu = new Students()
+            {
+                Age = 1,
+                DepIds = new int[] { 1, 2, 3 },
+                DepNames = new string[] { "wjf", "zyy" },
+                Name = "wjf"
+            };
+            context.Add(stu);
+            context.SaveChanges();
             var list = context.Set<Students>().ToList();
             context.SaveChanges();
         }
