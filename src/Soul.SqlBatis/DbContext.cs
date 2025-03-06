6ac0d85f8069ba161cf6b17f39727b946cacf735
王剑锋 <1448376744@qq.com>
2025-03-06 11:36:02 +0800

    dev

--- src/Soul.SqlBatis/DbContext.cs
+++ src/Soul.SqlBatis/DbContext.cs
@@ -131,7 +131,7 @@ namespace Soul.SqlBatis
 
         public IDbTransaction GetDbTransaction()
         {
-            return _transaction.DbTransaction;
+            return _transaction?.DbTransaction;
         }
 
 
