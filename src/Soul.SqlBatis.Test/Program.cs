ec90ddab593d2b4bd43611946d50f017c7d21eda
wjf <1448376744@qq.com>
2025-06-05 00:56:44 +0800

    dev

--- src/Soul.SqlBatis.Test/Program.cs
+++ src/Soul.SqlBatis.Test/Program.cs
@@ -13,22 +13,31 @@ DbContextSettings.Configure(DbType.Npgsql, configure =>
         return ((NpgsqlDataReader)record).GetFieldValue<int[]>(i);
     });
 });
-var context = new DbContext(configureOptions =>
+
+
+using (var context = new DbContext(configureOptions =>
 {
     configureOptions.UseLogger((sql, param) =>
     {
         Console.WriteLine(sql);
     });
     configureOptions.UseNpgsql(new NpgsqlConnection("Host=127.0.0.1;Port=5432;Username=postgres;Password=1024;Database=postgres;SSL Mode=Disable;"));
-});
-
-var gs = new List<Gender>();
-gs.Add(Gender.男);
-gs.Add(Gender.女);
-var pam = new DynamicParameters();
-pam.Add("@GS", gs);
-var entity = context.Set<Student>(pam)
-    .Where(a => DbOps.In(a.Gender, gs))
-    .AsTracking()
+}))
+{
+    var entity = context.Set<Student>()
     .ToList();
-Console.WriteLine();
+}
+
+using (var context = new DbContext(configureOptions =>
+{
+    configureOptions.UseLogger((sql, param) =>
+    {
+        Console.WriteLine(sql);
+    });
+    configureOptions.UseNpgsql(new NpgsqlConnection("Host=127.0.0.1;Port=5432;Username=postgres;Password=1024;Database=postgres;SSL Mode=Disable;"));
+}))
+{
+    var entity = context.Set<Student>()
+    .ToList();
+}
+
